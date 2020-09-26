3858cdecc9042c3d89d6ea624ebb0fd2eaf91336
nlhomme <9362475+nlhomme@users.noreply.github.com>
2020-09-26 12:07:59 +0200

    Initial Commit

---
+++ demo-compteur/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo-compteur
+//
+//  Created by Nicolas Lhomme on 26/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demo-compteur/demo_compteurApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_compteurApp.swift
+//  demo-compteur
+//
+//  Created by Nicolas Lhomme on 26/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct demo_compteurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
